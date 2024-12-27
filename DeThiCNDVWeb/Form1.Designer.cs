86830c045d77592342201ea77eaab36f22c558a8
clock <clock@WALDY-WIN>
2024-12-27 10:17:05 +0700

    Add project files.

---
+++ DeThiCNDVWeb/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace DeThiCNDVWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox_tenMatHang = new System.Windows.Forms.TextBox();
+            this.comboBox_nhaCungCap = new System.Windows.Forms.ComboBox();
+            this.comboBox_ngay = new System.Windows.Forms.ComboBox();
+            this.comboBox_thang = new System.Windows.Forms.ComboBox();
+            this.comboBox_nam = new System.Windows.Forms.ComboBox();
+            this.textBox_soLuong = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(208, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(726, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NHẬP THÔNG TIN HÀNG HOÁ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhà Cung Cấp:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Ngày Nhập:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(34, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 24);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Số Lượng:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(34, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 24);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Tên Mặt Hàng:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(463, 92);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 24);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Mã Mặt Hàng:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(463, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 24);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Đơn Vị Tính:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(463, 206);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 24);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Đơn Giá:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(463, 267);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 24);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Hình Ảnh:";
+            // 
+            // textBox_tenMatHang
+            // 
+            this.textBox_tenMatHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_tenMatHang.Location = new System.Drawing.Point(196, 145);
+            this.textBox_tenMatHang.Name = "textBox_tenMatHang";
+            this.textBox_tenMatHang.Size = new System.Drawing.Size(248, 29);
+            this.textBox_tenMatHang.TabIndex = 2;
+            // 
+            // comboBox_nhaCungCap
+            // 
+            this.comboBox_nhaCungCap.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox_nhaCungCap.FormattingEnabled = true;
+            this.comboBox_nhaCungCap.Location = new System.Drawing.Point(195, 89);
+            this.comboBox_nhaCungCap.Name = "comboBox_nhaCungCap";
+            this.comboBox_nhaCungCap.Size = new System.Drawing.Size(249, 32);
+            this.comboBox_nhaCungCap.TabIndex = 3;
+            // 
+            // comboBox_ngay
+            // 
+            this.comboBox_ngay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox_ngay.FormattingEnabled = true;
+            this.comboBox_ngay.Location = new System.Drawing.Point(195, 206);
+            this.comboBox_ngay.Name = "comboBox_ngay";
+            this.comboBox_ngay.Size = new System.Drawing.Size(74, 32);
+            this.comboBox_ngay.TabIndex = 4;
+            // 
+            // comboBox_thang
+            // 
+            this.comboBox_thang.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox_thang.FormattingEnabled = true;
+            this.comboBox_thang.Location = new System.Drawing.Point(283, 206);
+            this.comboBox_thang.Name = "comboBox_thang";
+            this.comboBox_thang.Size = new System.Drawing.Size(74, 32);
+            this.comboBox_thang.TabIndex = 4;
+            // 
+            // comboBox_nam
+            // 
+            this.comboBox_nam.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox_nam.FormattingEnabled = true;
+            this.comboBox_nam.Location = new System.Drawing.Point(370, 206);
+            this.comboBox_nam.Name = "comboBox_nam";
+            this.comboBox_nam.Size = new System.Drawing.Size(74, 32);
+            this.comboBox_nam.TabIndex = 4;
+            // 
+            // textBox_soLuong
+            // 
+            this.textBox_soLuong.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_soLuong.Location = new System.Drawing.Point(196, 264);
+            this.textBox_soLuong.Name = "textBox_soLuong";
+            this.textBox_soLuong.Size = new System.Drawing.Size(248, 29);
+            this.textBox_soLuong.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1119, 574);
+            this.Controls.Add(this.comboBox_nam);
+            this.Controls.Add(this.comboBox_thang);
+            this.Controls.Add(this.comboBox_ngay);
+            this.Controls.Add(this.comboBox_nhaCungCap);
+            this.Controls.Add(this.textBox_soLuong);
+            this.Controls.Add(this.textBox_tenMatHang);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "FormHangHoa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox_tenMatHang;
+        private System.Windows.Forms.ComboBox comboBox_nhaCungCap;
+        private System.Windows.Forms.ComboBox comboBox_ngay;
+        private System.Windows.Forms.ComboBox comboBox_thang;
+        private System.Windows.Forms.ComboBox comboBox_nam;
+        private System.Windows.Forms.TextBox textBox_soLuong;
+    }
+}
---
+++ DeThiCNDVWeb/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DeThiCNDVWeb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DeThiCNDVWeb")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b8c7151-b076-4741-b77d-835656337f0e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
